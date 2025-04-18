1757a81e4299a578b80615c6a74ad76100b23140
oscar ogilvie <osc.ogilvie@gmail.com>
2025-04-18 14:52:08 +0100

    testing and refactoring writer

--- src/writer.py
+++ src/writer.py
@@ -1,8 +1,10 @@
 def df_to_bytes(df, file_type):
     if file_type == "csv":
         file_data = df.to_csv(index=False)
+        file_data = bytes(file_data,"UTF-8")
     elif file_type == "json":
-        file_data = df.to_json(index=False)
+        file_data = df.to_json(orient="records")
+        file_data = bytes(file_data,"UTF-8")
     elif file_type == "parquet":
         file_data = df.to_parquet(index=False)
-    return bytes(file_data,"UTF-8")
+    return file_data
--- test/test_writer.py
+++ test/test_writer.py
@@ -1,7 +1,61 @@
 from src.writer import df_to_bytes
+import pandas as pd
 from pandas import DataFrame as df
 
+from io import BytesIO
+from moto import mock_aws
+from moto.core import patch_client
+import pytest
+import os
+import boto3
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+
+    """Mocked AWS Credentials for moto."""
+
+    os.environ['AWS_ACCESS_KEY_ID'] = 'test'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'test'
+    os.environ['AWS_SECURITY_TOKEN'] = 'test'
+    os.environ['AWS_SESSION_TOKEN'] = 'test'
+    os.environ['AWS_DEFAULT_REGION'] = 'eu-west-1'
+
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with mock_aws():
+        yield boto3.client('s3', region_name='eu-west-1')
+
+
 def test_writer_writes_to_bytes_of_csv():
-    Test_data = df({"TestA":["test1","test_2"],"TestB":["test1","test_2"]})
-    test_out = df_to_bytes(Test_data, "csv")
+    test_data = df({"TestA":["test1","test_2"],"TestB":["test1","test_2"]})
+    test_out = df_to_bytes(test_data, "csv")
     assert type(test_out) is bytes
+
+def test_writer_writes_to_bytes_of_json():
+    test_data = df({"TestA":["test1","test_2"],"TestB":["test1","test_2"]})
+    test_out = df_to_bytes(test_data, "json")
+    assert type(test_out) is bytes
+
+def test_writer_writes_to_bytes_of_parquet():
+    test_data = df({"TestA":["test1","test_2"],"TestB":["test1","test_2"]})
+    test_out = df_to_bytes(test_data, "parquet")
+    test_file = BytesIO(test_out)
+    assert type(test_out) is bytes
+    assert pd.read_parquet(test_file).equals(test_data)
+    
+def test_writer_outputs_can_put_to_bucket(s3):
+    patch_client(s3)
+    s3.create_bucket(
+        Bucket='test_bucket',
+        CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'}
+    )
+    test_data = df({"TestA":["test1","test_2"],"TestB":["test1","test_2"]})
+    test_csv = df_to_bytes(test_data, "csv")
+    test_json = df_to_bytes(test_data, "json")
+    test_parq = df_to_bytes(test_data, "parquet")
+    s3.put_object(Body=test_csv, Bucket="test_bucket", Key="test_csv.csv")
+    s3.put_object(Body=test_json, Bucket="test_bucket", Key="test_json.json")
+    s3.put_object(Body=test_parq, Bucket="test_bucket", Key="test_parquet.parquet")
+    bucket_contents = s3.list_objects_v2(Bucket='test_bucket')["Contents"]
+    assert len(bucket_contents) == len(["test_csv.csv","test_json.json","test_parquet.parquet"])
