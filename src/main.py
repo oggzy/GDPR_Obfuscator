a77fda30bbe005100e4ed2ca6668f1c190d4579b
oscar ogilvie <osc.ogilvie@gmail.com>
2025-04-23 17:35:56 +0100

    passing main test without imports

--- src/main.py
+++ src/main.py
@@ -1,15 +1,48 @@
-from src.obfuscator import obfuscate
-from src.reader import file_to_df
-from src.writer import df_to_bytes
 import json
+from pandas import DataFrame as df
+import awswrangler
 
 
 def GDPR_obfuscate(json_fields):
     inputted = json.loads(json_fields)
-    file_loc = json_fields["file_to_obfuscate"]
+    file_loc = inputted["file_to_obfuscate"]
     split_file = file_loc.split(".")
     file_type = split_file[-1]
     file_data = file_to_df(file_loc,file_type)
     obfuscated = obfuscate(inputted["pii_fields"], file_data)
     obfuscated_bytes = df_to_bytes(obfuscated,file_type)
     return obfuscated_bytes
+
+def df_to_bytes(df, file_type):
+    if file_type == "csv":
+        file_data = df.to_csv(index=False)
+        file_data = bytes(file_data,"UTF-8")
+    elif file_type == "json":
+        file_data = df.to_json(orient="records")
+        file_data = bytes(file_data,"UTF-8")
+    elif file_type == "parquet":
+        file_data = df.to_parquet(index=False)
+    return file_data
+
+def file_to_df(file_loc, file_type):
+    if file_type == "csv":
+        file_data = awswrangler.s3.read_csv(file_loc)
+    elif file_type == "json":
+        file_data = awswrangler.s3.read_json(file_loc)
+    elif file_type == "parquet":
+        file_data = awswrangler.s3.read_parquet(file_loc)
+    else:
+        raise InvalidFileType
+    return file_data
+
+class InvalidFileType(Exception):
+    "File Type not supported, obfuscate only supports csv, JSON, Parquet"
+    pass
+
+def obfuscate(pii_fields,file_df):
+    obfuscated_df = file_df.copy(deep=True)
+    obfuscated_df = obfuscated_df.drop(columns=pii_fields)
+    for column in pii_fields:
+        location = file_df.columns.get_loc(column)
+        obfuscated_df.insert(location, column, ["***" for i in range(len(file_df))])
+    return obfuscated_df
--- test/test_main.py
+++ test/test_main.py
@@ -1,17 +1,17 @@
 from src.main import GDPR_obfuscate
-import json
 from unittest.mock import patch
 
-def test_obfuscator_calls_funcs():
-    test_in = json.dumps("{'file_to_obfuscate': 's3://my_ingestion_bucket/new_data/file1.csv','pii_fields': ['name', 'email_address']}")
-    with patch('src.reader.file_to_df', return_value="read") as patch_read:
-        with patch('src.obfuscator.obfuscate', return_value="obfs") as patch_obfs:
-            with patch('src.writer.df_to_bytes', return_value="byte") as patch_byte:
-                test_out = GDPR_obfuscate(test_in)
-                test_args = []
-                test_args.append(patch_read.call_args.args)
-                test_args.append(patch_obfs.call_args.args)
-                test_args.append(patch_byte.call_args.args)    
+def test_obfuscate_calls_funcs():
+    test_in = """{"file_to_obfuscate": "s3://my_ingestion_bucket/new_data/file1.csv",
+    "pii_fields": ["name", "email_address"]}"""
+    with (patch('src.main.file_to_df', return_value="read") as patch_read,
+          patch('src.main.obfuscate', return_value="obfs") as patch_obfs,
+          patch('src.main.df_to_bytes', return_value="byte") as patch_byte):
+        test_out = GDPR_obfuscate(test_in)
+        test_args = []
+        test_args.append(patch_read.call_args.args)
+        test_args.append(patch_obfs.call_args.args)
+        test_args.append(patch_byte.call_args.args)
     assert test_out == "byte"
-    assert test_args == ["s3://my_ingestion_bucket/new_data/file1.csv","csv",["name", "email_address"],"read","obfs","csv"]
+    assert test_args == [("s3://my_ingestion_bucket/new_data/file1.csv","csv"),(["name", "email_address"],"read"),("obfs","csv")]
 
